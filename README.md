d402ec0eb2cd5ebadb845481943838dd8535d368
mehedihasanhrid1 <mehedihasanhrid1@gmail.com>
2023-10-18 01:45:42 +0600

    Initialize Client Side Repository of Taste Haven

---
+++ README.md
@@ -0,0 +1,5 @@
+# Taste Haven: A Journey of Flavors
+
+## Website Link: [Click Here]()
+
+## Server Link: [Click Here]()
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnCZRKW8uNuUkaABJfTdAmksvvTPec1F0",
+  authDomain: "taste-haven-d0082.firebaseapp.com",
+  projectId: "taste-haven-d0082",
+  storageBucket: "taste-haven-d0082.appspot.com",
+  messagingSenderId: "891528291025",
+  appId: "1:891528291025:web:c75c322f233d32e4975bf0"
+};
+
+const app = initializeApp(firebaseConfig);
