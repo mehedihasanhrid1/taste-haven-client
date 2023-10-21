b356ea30aac2095f7898d993bf57d6aa82599713
mehedihasanhrid1 <mehedihasanhrid1@gmail.com>
2023-10-21 10:01:09 +0600

    New Product Added Operation Handled Successfully

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,38 +1,67 @@
+import React, { useEffect, useState } from "react";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-  } from "@material-tailwind/react";
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
-   
-  export default function Cards() {
-    const navigate = useNavigate();
-    const handleSeeDetails = () => {
-      navigate(`/details`);
-    };
-    return (
-      <div className="mt-10 lg:mt-14 px-5 lg:px-10">
-        <div>
+
+export default function Cards() {
+  const [brands, setBrands] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/brands");
+      if (response.ok) {
+        const data = await response.json();
+        setBrands(data);
+      }
+    } catch (error) {
+      console.error("Error fetching brand data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSeeDetails = () => {
+    navigate(`/details`);
+  };
+
+  return (
+    <div className="mt-10 lg:mt-14 px-5 lg:px-10">
+      <div>
         <h3 className="mb-8 lg:mb-10 text-center text-3xl lg:text-5xl font-bold dark:text-neutral-200">
-        Our<span className='text-[#FB9333] dark:text-[#289944] ml-2 lg:ml-4'>Brands</span>
+          Our<span className='text-[#FB9333] dark:text-[#289944] ml-2 lg:ml-4'>Brands</span>
         </h3>
-        </div>
-       <div className="flex items-center justify-center flex-col md:flex-row">
-       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
-        <Card onClick={handleSeeDetails} className="w-80 lg:w-96 bg-gray-200 dark:bg-[#253246] hover:scale-105 duration-500">
-        <CardHeader floated={false}>
-          <img className="w-80 lg:w-96 object-cover" src="https://i.ibb.co/PwKnBXq/th-9.jpg" alt="profile-picture" />
-        </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h4" color="blue-gray" className="mb-2 dark:text-white">
-            Coca Cola
-          </Typography>
-        </CardBody>
-      </Card>
-        </div>
-       </div>
       </div>
-      
-    );
-  }
+      <div className="flex items-center justify-center flex-col md:flex-row">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+          {brands.map((brand, index) => (
+            <Card
+              onClick={handleSeeDetails}
+              className="cursor-pointer w-80 lg:w-96 bg-gray-200 dark:bg-[#253246] hover:scale-105 duration-500"
+              key={index}
+            >
+              <CardHeader floated={false}>
+                <img
+                  className="w-80 lg:w-96 h-60 object-cover"
+                  src={brand.image_url} 
+                  alt="brand-logo"
+                />
+              </CardHeader>
+              <CardBody className="text-center">
+                <Typography variant="h4" color="blue-gray" className="mb-2 dark:text-white">
+                  {brand.brandname} 
+                </Typography>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Addproduct.jsx
+++ src/pages/Addproduct.jsx
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddProduct() {
+  const [formData, setFormData] = useState({
+    product_name: '',
+    image: '',
+    brand_name: '',
+    product_category: '',
+    price: '',
+    rating: '',
+    description: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/products/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        
+        if (data.insertedId) {
+          toast.success("Product added successfully");
+
+          setTimeout(() => {
+                
+        setFormData({
+          product_name: '',
+          image: '',
+          brand_name: '',
+          product_category: '',
+          price: '',
+          rating: '',
+          description: '',
+        });
+
+          }, 3000);
+        } else {
+          console.error('Invalid response from the server');
+        }
+      } else {
+        console.error('Request failed with status', response.status);
+      }
+    } 
+    catch (error) {
+      console.error('Error adding product:', error);
+    }
+
+  };
   return (
     <section>
       <div className="py-8 px-5 lg:px-0 mx-auto max-w-2xl lg:pt-16 lg:pb-10">
-      <h3 className="mb-6 lg:mb-8 text-center text-2xl lg:text-4xl font-bold dark:text-neutral-200">
+        <h3 className="mb-6 lg:mb-8 text-center text-2xl lg:text-4xl font-bold dark:text-neutral-200">
           Add a new<span className='text-[#FB9333] dark:text-[#289944]'>{" "}Product</span>
         </h3>
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
-              <label htmlFor="name" className="block mb-2 text-sm md:text-base font-medium text-gray-900 dark:text-white">Product Name</label>
+              <label htmlFor="product_name" className="block mb-2 text-sm md:text-base font-medium text-gray-900 dark:text-white">Product Name</label>
               <input
                 type="text"
-                name="name"
-                id="name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                name="product_name" 
+                id="product_name"
+                value={formData.product_name}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
                 placeholder="Type product name"
                 required
               />
@@ -24,18 +85,23 @@ function AddProduct() {
               <label htmlFor="image" className="block mb-2 text-sm md:text-base font-medium text-gray-900 dark:text-white">Image</label>
               <input
                 type="text"
-                name="image"
+                name="image" 
                 id="image"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                value={formData.image}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
                 placeholder="Type image url"
                 required
               />
             </div>
             <div className="w-full">
-              <label htmlFor="brand" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Brand</label>
+              <label htmlFor="brand_name" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Brand</label>
               <select
-                id="brand"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                id="brand_name"
+                name='brand_name' 
+                value={formData.brand_name}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
               >
                 <option defaultValue="Select Category">Select Brand</option>
                 <option value="Coca Cola">Coca Cola</option>
@@ -50,27 +116,33 @@ function AddProduct() {
               <label htmlFor="price" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Price</label>
               <input
                 type="number"
-                name="price"
+                name="price" 
                 id="price"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                value={formData.price}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
                 placeholder="$10"
                 required
+                step="0.01"
               />
             </div>
             <div>
-              <label htmlFor="category" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Category</label>
+              <label htmlFor="product_category" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Category</label>
               <select
-                id="category"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                id="product_category"
+                name='product_category' 
+                value={formData.product_category}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
               >
                 <option defaultValue="Select Category">Select category</option>
-                <option value="Food">Food</option>
+                <option value="Soda">Soda</option>
                 <option value="Coffee">Coffee</option>
-                <option value="Grocery">Grocery</option>
-                <option value="Milk">Milk</option>
-                <option value="Noodles">Noodles</option>
-                <option value="Soft Drinks">Soft Drinks</option>
-                <option value="Burger">Burger</option>
+                <option value="First Food">First Food</option>
+                <option value="Snacks">Snacks</option>
+                <option value="Beverage">Beverage</option>
+                <option value="Frozen Food">Frozen Food</option>
+                <option value="Tea">Tea</option>
                 <option value="Cocolate">Cocolate</option>
               </select>
             </div>
@@ -78,31 +150,39 @@ function AddProduct() {
               <label htmlFor="rating" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Rating</label>
               <input
                 type="number"
-                name="rating"
+                name="rating" 
                 id="rating"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                value={formData.rating}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
                 placeholder="5"
                 required
+                min={1}
+                max={5}
               />
             </div>
             <div className="sm:col-span-2">
               <label htmlFor="description" className="block mb-2 text-sm lg:text-base font-medium text-gray-900 dark:text-white">Description</label>
               <textarea
                 id="description"
+                name='description' 
+                value={formData.description}
+                onChange={handleChange}
                 rows="8"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white"
                 placeholder="Your description here"
               ></textarea>
             </div>
           </div>
           <button
             type="submit"
-            className="inline-flex items-center mt-4 sm:mt-6 font-medium py-3 px-5 lg:px-8  text-center rounded-lg bg-[#289944] sm:w-fit hover:bg-[#248a3e] focus:ring-2 focus:outline-none focus:ring-[#36cd5c]  dark:focus:ring-[#f99e48] text-white dark:bg-[#FB9333] dark:hover:bg-[#dd7614]"
+            className="inline-flex items-center mt-4 sm:mt-6 font-medium py-3 px-5 lg:px-8 text-center rounded-lg bg-[#289944] sm:w-fit hover-bg-[#248a3e] focus-ring-2 focus-outline-none focus-ring-[#36cd5c] dark-focus-ring-[#f99e48] text-white dark-bg-[#FB9333] dark-hover-bg-[#dd7614]"
           >
             Add product
           </button>
         </form>
       </div>
+      <ToastContainer autoClose={2000}/>
     </section>
   );
 }
